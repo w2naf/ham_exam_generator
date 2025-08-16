5d56b37be9362dd5b3631c24a102ebacf66fcf05
Nathaniel A. Frissell <nathaniel.frissell@scranton.edu>
2025-08-16 13:05:33 -0400

    I think it is working

--- generate_brightspace_quiz.py
+++ generate_brightspace_quiz.py
@@ -6,6 +6,8 @@ from collections import defaultdict
 from pathlib import Path
 from datetime import datetime
 import shutil
+import glob
+import os
 
 # Files relative to this script
 BASE_DIR = Path(__file__).parent
@@ -52,26 +54,37 @@ def main():
     if QUIZ_DIR.exists():
         shutil.rmtree(QUIZ_DIR)
         print(f"Deleted existing quiz directory: {QUIZ_DIR}")
-    
     QUIZ_DIR.mkdir(parents=True, exist_ok=True)
+
+    # Copy over figure files and keep track of them in a dictionary.
+    figure_files = glob.glob(str(BASE_DIR / "pool" / "*.jpg"))
+    figs_dct = {}
+    for figure_file in figure_files:
+        shutil.copy(figure_file, QUIZ_DIR)
+        print(f"Copied figure file: {figure_file} to {QUIZ_DIR}")
+
+        bname    = os.path.basename(figure_file)
+        if "T1" in bname:
+            fig_name = "figure T-1"
+        elif "T2" in bname:
+            fig_name = "figure T-2"
+        elif "T3" in bname:
+            fig_name = "figure T-3"
+
+        figs_dct[fig_name] = bname
 
     # Initialze Subelement Quiz Files
     sub_els = {}
     for inx in range(10):
         sub_el  = f'T{inx}'
         sub_csv = QUIZ_DIR / f'{sub_el}.csv'
+        sub_els[sub_el] = {'csv':sub_csv,'count':0}
 
-        sub_els[sub_el] = {'csv':sub_csv}
-
-        # with open(sub_csv, 'w', encoding='utf-8') as f:
-        #     f.write("")
-        print(sub_csv)
-
-    for qq in pool:
-        print(qq)
-        
+    fig_qIDs = []
+    for qq in pool:      
         qID      = qq.get("id")
         sub_el   = qID[0:2]
+        
         question = f"[{qID}] " + qq.get("question")
 
         answers   = qq.get("answers")
@@ -87,22 +100,49 @@ def main():
         tta(f"QuestionText,{question},,,")
         tta("Points,1,,,")
         tta("Difficulty,1,,,")
-        # tta("Image,images/MC1.jpg,,,")
-        # tta("Image,,,,")
         for corr, ans in zip(correct, answers):
             tta(f"Option,{corr},{ans},,,")
+
+        has_fig = False
+        for fig_name,fig in figs_dct.items():
+            if fig_name in question:
+                # tta("Image,images/MC1.jpg,,,")
+                # tta("Image,,,,")
+                tta(f"Image,images/{fig},,,")
+                has_fig = True
+                fig_qIDs.append(qID)
+                break
+        
         tta("Hint,,,,")
         tta("Feedback,,,,")
         tta("")
 
-        for ll in tt:
-            print(ll)
-
         with open(sub_els[sub_el]['csv'], 'a', encoding='utf-8') as f:
             f.write("\n".join(tt))
             f.write("\n")
+       
+        sub_els[sub_el]['count'] += 1
 
-        print("Added question to subelement:", sub_el)
+        # # print(qq)
+        # for ll in tt:
+        #     print(ll)
+
+        # print("Added question to subelement:", sub_el)
+
+    print()
+    print("########################################")
+    print("All quiz files generated in:", QUIZ_DIR)
+    print()
+    print("Total questions generated:", len(pool))
+    for sub_el, data in sub_els.items():
+        print(f"  - {sub_el}: {data['count']} questions")
+
+    print()
+    if len(fig_qIDs) > 0:
+        print(f"WARNING: The following {len(fig_qIDs)} questions contain figures.")
+        print("Be sure to upload the *.jpgs to the images/ directory of your BrightSpace Class.")
+        for qID in fig_qIDs:
+            print(f"  - {qID}")
 
 if __name__ == "__main__":
     main()
